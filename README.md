3a3dac52e3f27e18adf231af8b5d626266ed6d87
Adrian Statescu <mergesortv@gmail.com>
2013-03-09 23:39:10 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# express-tweets
+
+Book Node.js Up and Running - Part 1 => Chapter 2 Doing Interesting Things => Let's Build Twitter (working code for Express 3.0 and ejb without partial)
+
+# Installation
+
+$ node install express
+$ node install ejb
+$ node server.js
+
+# Usage
+
+Open a browser and go to http://localhost:8080
+
+
+
---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var sys = require("sys");
+
+var app = express()
+
+app.listen(8080)
+
+sys.puts("Server listen on port:8080")
+
+//tell to render layout with ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+var tweets = []
+
+app.get('/', function(request, response){
+
+        var title = 'Express - Web Application framework for node | EJS - Embedded JavaScript renderer for node',
+            header = 'Welcome to Express';
+
+            response.render('layout', {
+                  locals: {
+                      'title': title,
+                      'header': header,
+                      'tweets': tweets,
+                      'stylesheet': ['/style.css']
+                  } 
+            })
+})
+
+app.get('/tweets', function(request, response){
+ 
+       response.send( tweets )
+})
+
+app.post('/send', express.bodyParser(), function(request, response){
+
+        if(request.body && request.body.tweet) {
+
+          tweets.push(request.body.tweet)  
+
+           if(acceptsHTML(request.headers['accept']) ) {
+
+             response.redirect("/", 303)
+
+           } else {
+
+             response.send({"status":"Ok","message": "Tweet received"})  
+           }
+
+        } else {
+
+          response.send({"status":"nok","message": "No Tweet received"})  
+        }
+ 
+})
+
+
+function acceptsHTML( header ) {
+
+    var accepts = header.split(',')
+
+    for(var i=0,j=accepts.length; i<j; i++) {
+
+        if(accepts[i] == 'text/html') return true;
+    } 
+
+  return false;
+}
